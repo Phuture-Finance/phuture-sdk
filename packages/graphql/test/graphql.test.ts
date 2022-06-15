a5b04e42533da11c6ce6907dd50c5b0ab456f5bf
Jay <jmartinsmith@outlook.com>
2022-06-15 16:28:37 +0100

    added all phuture (not lm) queries + tests

--- packages/graphql/test/graphql.test.ts
+++ packages/graphql/test/graphql.test.ts
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {phutureGraphQl} from '../src';
+import {phutureGraphQl, lmGraphQl} from '../src';
 
 const userId = '0x000000000000000000000000000000000000dead';
 /** @test {phutureGraphQl} */
@@ -50,9 +50,72 @@ test('successfully fetch daily capitalisations', async () => {
 test('unsuccessfully fetch daily capitalisations', async () => {
 	// Setup
 	// execute
-	const {dailyCapitalizations} = await phutureGraphQL().getCapitalizations({
+	const {dailyCapitalizations} = await phutureGraphQl().getCapitalizations({
 		timestamp_gte: '9000000000000000000000000000000',
 	});
 	// Verify
 	expect(dailyCapitalizations.length).to.equal(0);
 });
+
+test('fetch portfolio data', async () => { 
+	// Setup
+	// execute
+	const {users} = await phutureGraphQl().getPortfolioData({ id: userId });
+    
+	// Verify
+	expect(users[0].indexes.length).not.to.equal(0);
+})
+
+test('chart data', async () => { 
+	// Setup
+    const variables = { 
+        id: "0xf9ccb834adbe4591fd517aa69a24bf97d1386092",
+        dateEnd:1653004800,
+        dateNow: 1652745600
+    }
+	// execute
+	const {index} = await phutureGraphQl().getChartData(variables);
+    
+	// Verify
+	expect(index).not.to.be.undefined;
+})
+
+test('fetch daily cap', async () => { 
+	// Setup
+	// execute
+	const {dailyCapitalizations} = await phutureGraphQl().getDailyCap({ date: 0 });
+    
+	// Verify
+	expect(dailyCapitalizations.length).not.to.equal(0);
+})
+
+test('fetch index info', async () => { 
+	// Setup
+	// execute
+	const {index} = await phutureGraphQl().getIndexInfo({id: "0xf9ccb834adbe4591fd517aa69a24bf97d1386092" });
+    
+	// Verify
+	expect(index).not.to.be.undefined;
+})
+
+test('get reserves', async () => { 
+
+    // Setup
+	// execute
+	const {reserves} = await lmGraphQl().getReserves();
+    
+	// Verify
+	expect(reserves).not.to.be.undefined;
+})
+getRangesByAddress
+
+
+test('get ranges by address', async () => { 
+
+    // Setup
+	// execute
+	const {vestingRanges} = await lmGraphQl().getRangesByAddress({address: ""});
+    
+	// Verify
+	expect(reserves).not.to.be.undefined;
+})
