594b8ef64d6b46ebe6be59d7d288ca413395e390
Jay <jmartinsmith@outlook.com>
2022-06-29 11:47:18 +0100

    Added documentation to the examples package

--- examples/mint/src/mint-swap-value.ts
+++ examples/mint/src/mint-swap-value.ts
@@ -1,39 +1,51 @@
-import { ZeroExAggregator } from "@phuture/0x-aggregator";
-import { Index } from "@phuture/index";
-import { IndexRouter } from "@phuture/index-router";
-import "dotenv/config";
-import { BigNumber, ethers, utils } from "ethers";
-import * as process from "process";
+import {ZeroExAggregator} from '@phuture/0x-aggregator';
+import {Index} from '@phuture/index';
+import {IndexRouter} from '@phuture/index-router';
+import 'dotenv/config';
+import {BigNumber, ethers, utils} from 'ethers';
+import * as process from 'node:process';
 
-const amountToSellDesired = BigNumber.from(utils.parseEther("1"));
+/**
+ * This example looks at minting 1ETH
+ */
+
+const amountToSellDesired = BigNumber.from(utils.parseEther('1'));
 
 async function main() {
 	const privateKey = process.env.PRIVATE_KEY;
-	if (!privateKey) throw new Error("PRIVATE_KEY is not set");
+	if (!privateKey) throw new Error('PRIVATE_KEY is not set');
 
 	const nodeUrl = process.env.NODE_URL;
-	if (!nodeUrl) throw new Error("NODE_URL is not set");
+	if (!nodeUrl) throw new Error('NODE_URL is not set');
 
 	const wallet = new ethers.Wallet(
 		privateKey,
-		new ethers.providers.JsonRpcProvider(nodeUrl)
+		new ethers.providers.JsonRpcProvider(nodeUrl),
 	);
 
-	const index = new Index(wallet, "0x778b8cc9d9d8e97ab7f6e100e45c1e576bb1d6d4");
+	const index = new Index(wallet, '0x778b8cc9d9d8e97ab7f6e100e45c1e576bb1d6d4');
 
-	const { amounts, amountToSellQuoted } = await index.scaleAmount(
-		amountToSellDesired
+	/**
+	 * The index will be made up of multiple tokens, and their weighted value might not be the same.
+	 * So to get the correct amounts, we call scaleAmount
+	 */
+	const {amounts, amountToSellQuoted} = await index.scaleAmount(
+		amountToSellDesired,
 	);
 
 	const zeroEx = new ZeroExAggregator();
 
+	/**
+	 * After we have the correctly scaled amounts, we can then get quotes for each amount.
+	 * This will return a {@see Zero0xQuoteResponse} that can be passed straight into the mint method
+	 */
 	const quotes = await Promise.all(
 		Object.entries(amounts).map(async ([asset, amount]) => {
 			const {
 				buyAmount: buyAssetMinAmount,
 				to: swapTarget,
 				data: assetQuote,
-			} = await zeroEx.quote("ETH", asset, amount);
+			} = await zeroEx.quote('ETH', asset, amount);
 
 			return {
 				asset,
@@ -41,21 +53,25 @@ async function main() {
 				buyAssetMinAmount,
 				assetQuote,
 			};
-		})
+		}),
 	);
 
 	const indexRouter = new IndexRouter(
 		wallet,
-		"0xb3a9b7b5728416227cb09d047fae2df36df04819"
+		'0xb3a9b7b5728416227cb09d047fae2df36df04819',
 	);
 
+	/**
+	 * Once we have our quotes in the form of an array of {@see Zero0xQuoteResponse}
+	 * We can then perform the mint.
+	 */
 	await indexRouter.mint(
 		{
 			index: index.address,
 			recipient: wallet.address,
 			quotes,
 		},
-		amountToSellQuoted
+		amountToSellQuoted,
 	);
 }
 
