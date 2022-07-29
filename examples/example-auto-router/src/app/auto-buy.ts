86783a00836edac4bbee59c2f922153f0a27ca81
Eugene Lepeico <eugenelepeico@gmail.com>
2022-07-29 18:02:49 +0300

    fix: auto buy sell example (#36)

--- examples/example-auto-router/src/app/auto-buy.ts
+++ examples/example-auto-router/src/app/auto-buy.ts
@@ -1,6 +1,14 @@
-import { BigNumber } from 'ethers';
-import { autoRouter, index } from './common';
+import {BigNumber} from 'ethers';
+import {account, autoRouter, index} from './common';
+import {Erc20Permit} from "@phuture/erc-20";
 
 export default async function autoBuy(amountToSellDesired: BigNumber) {
-	await autoRouter.autoBuy(index, amountToSellDesired);
+	const tokenAddress = process.env['TOKEN_ADDRESS'];
+	if (tokenAddress) {
+		await autoRouter.autoBuy(index, amountToSellDesired);
+	}
+
+	const token = new Erc20Permit(account, tokenAddress);
+
+	await autoRouter.autoBuy(index, amountToSellDesired, token);
 }
