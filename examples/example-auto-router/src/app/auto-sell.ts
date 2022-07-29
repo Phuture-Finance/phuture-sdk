e11aa2ea482856601b7dad0225f0213230817be3
Eugene Lepeico <eugenelepeico@gmail.com>
2022-07-29 18:01:23 +0300

    fix: auto buy sell example

--- examples/example-auto-router/src/app/auto-sell.ts
+++ examples/example-auto-router/src/app/auto-sell.ts
@@ -1,6 +1,14 @@
 import { BigNumber } from 'ethers';
-import { autoRouter, index } from './common';
+import {account, autoRouter, index} from './common';
+import {Erc20Permit} from "@phuture/erc-20";
 
 export default async function autoSell(amountToSellDesired: BigNumber) {
-	await autoRouter.autoSell(index, amountToSellDesired);
+	const tokenAddress = process.env['TOKEN_ADDRESS'];
+	if (tokenAddress) {
+		await autoRouter.autoSell(index, amountToSellDesired);
+	}
+
+	const token = new Erc20Permit(account, tokenAddress);
+
+	await autoRouter.autoSell(index, amountToSellDesired, token);
 }
--- examples/example-auto-router/src/app/common.ts
+++ examples/example-auto-router/src/app/common.ts
@@ -1,15 +1,9 @@
-import { ethers } from 'ethers';
-import {
-	Account,
-	AutoRouter,
-	Index,
-	IndexRouter,
-	ZeroExAggregator,
-} from '@phuture/sdk';
+import {ethers} from 'ethers';
+import {Account, AutoRouter, Index, IndexRouter, ZeroExAggregator,} from '@phuture/sdk';
 import 'dotenv/config';
-import { getEnv } from './utils';
+import {getEnv} from './utils';
 
-const account = new Account(
+export const account = new Account(
 	new ethers.Wallet(
 		getEnv('PRIVATE_KEY'),
 		new ethers.providers.JsonRpcProvider(getEnv('NODE_URL'))
