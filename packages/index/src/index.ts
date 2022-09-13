d62f9eb27d8462e54f5c1211f261804887eb44e8
Eugene Lepeico <eugenelepeico@gmail.com>
2022-09-13 11:48:25 +0300

    feat: index pricer

--- packages/index/src/index.ts
+++ packages/index/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/base-index';
+export * from './lib/default-index-pricer';
+export * from './lib/index-pricer';
 export * from './lib/interfaces';
 export * from './lib/subraph.repository';
