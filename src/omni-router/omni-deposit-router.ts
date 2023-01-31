047322844d28aeb413856887d5d7a3540c8cbc3d
Jay <jmartinsmith@outlook.com>
2023-01-31 13:31:18 +0000

    add deposit method

--- src/omni-router/omni-deposit-router.ts
+++ src/omni-router/omni-deposit-router.ts
@@ -5,17 +5,43 @@ import {
 } from '../typechain'
 import { Contract } from 'contract'
 import { Account } from 'account'
+import { PromiseOrValue } from 'typechain/common'
+
+import { BigNumberish, ContractTransaction } from 'ethers'
 
 export class OmniDepositRouter extends Contract<OmniRouterInterface> {
   /**
    * ### Creates a new OmniDepositRouter instance
    *
    * @param account Account to use for signing
-   * @param contract Contract instance or address of the IndexDepositRouter contract
+   * @param contract Contract instance or address of the OmniRouterInterface contract
    *
-   * @returns New IndexDepositRouter token instance
+   * @returns New OmniRouterInterface token instance
    */
   constructor(account: Account, contract: OmniRouterInterface | Address) {
     super(account, contract, OmniRouter__factory)
+  }
+
+  /**
+   * ### Deposit
+   *
+   * @param index buy token address
+   * @param sellAmount sell token's  amount
+   * @param quotes (optional) quotes for deposits with USDC
+   *
+   * @returns deposit transaction
+   */
+  async deposit(
+    index: PromiseOrValue<string>,
+    sellAmount: BigNumberish,
+  ): Promise<ContractTransaction> {
+    const estimatedNativeGas = await this.contract.estimateGas.deposit(
+      index,
+      this.account.address(),
+    )
+    return this.contract.deposit(index, this.account.address(), {
+      value: sellAmount,
+      gasLimit: estimatedNativeGas,
+    })
   }
 }
