57c729ce38b85e80f171c70762a06f11f14c9118
Gulikovskiy <ivan_moroz@outlook.com>
2023-03-16 11:46:08 +0200

    fix: tests

--- src/tests/service.test.ts
+++ src/tests/service.test.ts
@@ -1,48 +1,56 @@
+import { Message } from '@layerzerolabs/scan-client'
+
 import { createOmniTransactionService } from '../omni-transaction-service'
 import { MessageStatus } from '../omni-transaction-service/utils'
 
 const mockTestStatus = (
-  isDstTx?: boolean,
-  isStatusSuccess?: boolean,
-  isMatic?: boolean,
+  isDstTx: boolean,
+  isStatusSuccess: boolean,
   isTxFailed?: boolean,
-) => [
-  {
-    created: 1677580103,
-    dstChainId: isMatic ? 109 : 10121,
-    dstTxHash: isTxFailed
-      ? '0x791ab18bcfb2a719fe71998f41be540ac3f8c0d2c1a53dfa820f88b4363db970'
-      : isDstTx
-      ? '0x728ad57658b7789347e32856af404949ef200b28c0e5662c16d140af6adb03df'
-      : undefined,
-    dstUaAddress: '0x4cfe64f6a7a2a382323bae2be883c10964bdb737',
-    srcBlockHash:
-      '0x4879125caf96f109de6d91e7485e6c1e7ea3e3bb9c6f7662f28349f337daf852',
-    srcBlockNumber: '9447087',
-    srcChainId: 10143,
-    srcTxHash:
-      '0x5c1579e481904b6b601a8655bd632bcb1786564a13e0c540ed49fd879299779e',
-    srcUaAddress: '0x76e1dbaee729a215509103c0c1dd6d349240642b',
-    srcUaNonce: 25,
-    status: isStatusSuccess ? MessageStatus.DELIVERED : MessageStatus.INFLIGHT,
-    updated: 1677580247,
-  },
-]
+  isEmpty = false,
+) =>
+  !isEmpty
+    ? [
+        {
+          created: 1677580103,
+          dstChainId: 10121,
+          dstTxHash: isTxFailed
+            ? '0x791ab18bcfb2a719fe71998f41be540ac3f8c0d2c1a53dfa820f88b4363db970'
+            : isDstTx
+            ? '0x728ad57658b7789347e32856af404949ef200b28c0e5662c16d140af6adb03df'
+            : undefined,
+          dstUaAddress: '0x4cfe64f6a7a2a382323bae2be883c10964bdb737',
+          srcBlockHash:
+            '0x4879125caf96f109de6d91e7485e6c1e7ea3e3bb9c6f7662f28349f337daf852',
+          srcBlockNumber: '9447087',
+          srcChainId: 10143,
+          srcTxHash:
+            '0x5c1579e481904b6b601a8655bd632bcb1786564a13e0c540ed49fd879299779e',
+          srcUaAddress: '0x76e1dbaee729a215509103c0c1dd6d349240642b',
+          srcUaNonce: 25,
+          status: isStatusSuccess
+            ? MessageStatus.DELIVERED
+            : isTxFailed
+            ? MessageStatus.FAILED
+            : MessageStatus.INFLIGHT,
+          updated: 1677580247,
+        },
+      ]
+    : ([] as unknown as Message[])
 
 test('Case with all "DELIVERED" statuses', async () => {
   const client = createOmniTransactionService({
     client: {
       getMessagesBySrcTxHash: async () => ({
-        messages: mockTestStatus(true, true, false),
+        messages: mockTestStatus(true, true),
       }),
     },
-    maticApiKey: 'HRY5PB899EMPXQ38Y2EHZVRA44CBF1MC73',
-    ethApiKey: '1CRQMKJVWNBV5QJ5X1P7JRYTWR5G3AHRMW',
   })
 
   const result = await client.getRemoteTransactionStatuses(
     '0x5c1579e481904b6b601a8655bd632bcb1786564a13e0c540ed49fd879299779e',
   )
+  console.log('result: ', result)
 
   expect(result.homeToRemote[0].status).toBe(MessageStatus.DELIVERED)
   expect(result.remoteToHome[0].status).toBe(MessageStatus.DELIVERED)
@@ -54,11 +62,9 @@ test.failing(
     const client = createOmniTransactionService({
       client: {
         getMessagesBySrcTxHash: async () => ({
-          messages: mockTestStatus(true, true, false),
+          messages: mockTestStatus(true, true),
         }),
       },
-      maticApiKey: 'fail',
-      ethApiKey: 'fail',
     })
 
     const result = await client.getRemoteTransactionStatuses('test')
@@ -71,11 +77,9 @@ test('Case when home-to-remote TX still wasn`t created ', async () => {
   const client = createOmniTransactionService({
     client: {
       getMessagesBySrcTxHash: async () => ({
-        messages: mockTestStatus(false, true, false),
+        messages: mockTestStatus(false, true),
       }),
     },
-    maticApiKey: 'HRY5PB899EMPXQ38Y2EHZVRA44CBF1MC73',
-    ethApiKey: '1CRQMKJVWNBV5QJ5X1P7JRYTWR5G3AHRMW',
   })
 
   const result = await client.getRemoteTransactionStatuses('test')
@@ -87,30 +91,9 @@ test('Case when remote-to-home TX still wasn`t created ', async () => {
   const client = createOmniTransactionService({
     client: {
       getMessagesBySrcTxHash: async () => ({
-        messages: mockTestStatus(false, true, false),
+        messages: mockTestStatus(false, true, false, true),
       }),
     },
-    maticApiKey: 'fail',
-    ethApiKey: 'fail',
-  })
-
-  const result = await client.getRemoteTransactionStatuses(
-    '0x5c1579e481904b6b601a8655bd632bcb1786564a13e0c540ed49fd879299779e',
-  )
-
-  expect(result.homeToRemote[0].status).toBe(MessageStatus.INFLIGHT)
-  expect(result.remoteToHome[0].status).toBe(MessageStatus.INFLIGHT)
-})
-
-test('Case when one of the transactions was on matic network', async () => {
-  const client = createOmniTransactionService({
-    client: {
-      getMessagesBySrcTxHash: async () => ({
-        messages: mockTestStatus(true, true, true),
-      }),
-    },
-    maticApiKey: 'HRY5PB899EMPXQ38Y2EHZVRA44CBF1MC73',
-    ethApiKey: '1CRQMKJVWNBV5QJ5X1P7JRYTWR5G3AHRMW',
   })
 
   const result = await client.getRemoteTransactionStatuses(
@@ -125,11 +108,9 @@ test('Case when one of the transactions was failed', async () => {
   const client = createOmniTransactionService({
     client: {
       getMessagesBySrcTxHash: async () => ({
-        messages: mockTestStatus(true, true, false, true),
+        messages: mockTestStatus(true, false, true),
       }),
     },
-    maticApiKey: 'HRY5PB899EMPXQ38Y2EHZVRA44CBF1MC73',
-    ethApiKey: '1CRQMKJVWNBV5QJ5X1P7JRYTWR5G3AHRMW',
   })
 
   const result = await client.getRemoteTransactionStatuses(
